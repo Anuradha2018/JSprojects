c02c879df8c9b8d4e1f7c8b04c65c74eae378667
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-10-24 22:16:03 +0200

    css added,
    Javascript for buttons and rest of HTML added

--- TodoApp/js/main.js
+++ TodoApp/js/main.js
@@ -3,6 +3,10 @@
 document.getElementById('add').addEventListener('click', function() {
     var value = document.getElementById('item').value;
     if(value) {
-        
+        addItemTodo(value);
     }
 });
+
+function addItemTodo(item) {
+    console.log(item);
+}
