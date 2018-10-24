8fe42bb3fff66e835d678d67500bfcb4460a2302
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-10-24 23:20:06 +0200

    add button implementation added

--- TodoApp/js/main.js
+++ TodoApp/js/main.js
@@ -1,12 +1,36 @@
 
+let removeIcon='<i style="color:red" class="far fa-trash-alt"></i>';
+let completeIcon='<i style="color:green" class="far fa-check-circle"></i>';
 
 document.getElementById('add').addEventListener('click', function() {
-    var value = document.getElementById('item').value;
-    if(value) {
+    let value = document.getElementById("item").value;
+    if(value) { 
         addItemTodo(value);
     }
 });
 
-function addItemTodo(item) {
-    console.log(item);
+function addItemTodo(text) {
+    let list = document.getElementById('complete');
+
+    let item = document.createElement('li');
+    item.innerText = text;
+
+    let buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+
+
+    let remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeIcon;
+
+    let complete = document.createElement('button');
+    complete.classList.add('complete')
+    complete.innerHTML = completeIcon;
+    list.appendChild(item);
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    
 }
