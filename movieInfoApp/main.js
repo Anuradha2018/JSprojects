3bf7749560a082ff98c13077bdb0eaa1579f0fd8
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-10-30 21:35:15 +0100

    movie view updated

--- movieInfoApp/main.js
+++ movieInfoApp/main.js
@@ -30,4 +30,43 @@ $(document).ready(() => {
         console.log(err);
       });
   }
-  
+  function movieSelected(id) {
+      sessionStorage.setItem('movieId', id);
+      window.location= 'movie.html';
+      return false;
+  }
+  function getMovie() {
+      let movieId= sessionStorage.getItem ('movieId');
+
+      function getMovies(searchText){
+        axios.get('http://www.omdbapi.com/?apikey=39bea9fe&i='+smovieId)
+          .then((response) => {
+            console.log(response);
+            let movie = response.data;
+
+            let output =`
+            <div class ="row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                    </ul>
+                </div>
+                </div>
+
+            `;
+            $('#movie').html(output);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+  }
